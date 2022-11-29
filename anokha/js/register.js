27e9f16de96d50abc0096f16f70cc7a802ed52f5
Madhav Harikumar <92919612+madhavh10@users.noreply.github.com>
2022-11-29 23:10:34 +0530

    js uploaded

---
+++ anokha/js/register.js
@@ -0,0 +1,67 @@
+function myFunction() 
+{
+    var x = document.getElementById("myDIV");
+    if (x.innerHTML === "Id") {
+        x.innerHTML = "Your Team Id is 101";
+    } 
+    else 
+    {
+        x.innerHTML = "Id";
+    }
+}
+
+function NameCheck() 
+{
+    var x = document.getElementById("tname1").value;
+    var y = x.split("")
+    let sp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    var arrLen = sp.length;
+    for (var i = 0; i < arrLen; i++) 
+    {
+        if (y.includes(sp[i])) 
+        {
+            alert("Please Remove special characters")
+        }
+    }
+}
+
+function phoneCheck()
+{
+    var x = document.getElementById("contact").value;
+    if(x.length()<10)
+    {
+        alert("Not a valid number")
+    }
+}
+
+function placeCheck() 
+{
+    var x = document.getElementById("place").value;
+    var y = x.split("")
+    let sp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    var arrLen = sp.length;
+    for (var i = 0; i < arrLen; i++) 
+    {
+        if (y.includes(sp[i])) 
+        {
+            alert("Please Remove special characters")
+        }
+    }
+}
+
+function stateCheck() 
+{
+    var x = document.getElementById("state").value;
+    var y = x.split("")
+    let sp = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'];
+    var arrLen = sp.length;
+    for (var i = 0; i < arrLen; i++) 
+    {
+        if (y.includes(sp[i])) 
+        {
+            alert("Please Remove special characters")
+        }
+    }
+}
+
+
